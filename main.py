e36d221fd37f9bd55819ee0c32a165ef6a3d2a82
AVerz26 <118853023+AVerz26@users.noreply.github.com>
2024-01-20 16:01:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -53,9 +53,12 @@ def visualize_clusters(X_scaled, cluster_col, optimal_k, centroids, scaler):
     # Reverter a escala original dos dados
     X_original_scale = scaler.inverse_transform(X_scaled)
 
+    # Reverter a escala original dos centroides
+    centroids_original_scale = scaler.inverse_transform(centroids)
+
     # Criar o plot usando matplotlib
     plt.scatter(X_original_scale[:, 0], X_original_scale[:, 1], c=cluster_col, cmap='viridis', label='Dados')
-    plt.scatter(centroids[:, 0], centroids[:, 1], c='red', marker='X', s=100, label='Centroids')
+    plt.scatter(centroids_original_scale[:, 0], centroids_original_scale[:, 1], c='red', marker='X', s=100, label='Centroids')
     plt.title(f'Clusters identificados pelo K-means (k={optimal_k})')
     plt.xlabel('Feature 1 (age)')
     plt.ylabel('Feature 2 (income)')
@@ -63,6 +66,7 @@ def visualize_clusters(X_scaled, cluster_col, optimal_k, centroids, scaler):
     st.set_option('deprecation.showPyplotGlobalUse', False)
     # Incorporar o plot no Streamlit
     st.pyplot()
+
 
 if __name__ == "__main__":
     main()
